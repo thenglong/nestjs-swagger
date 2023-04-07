b624a9d5890f92a52470d4281dd51ce41f898e37
thenglong <thenglongheng2k@gmail.com>
2023-04-07 10:02:42 +0700

    feat: add support for multiple response with the same status code

--- lib/services/mimetype-content-wrapper.ts
+++ lib/services/mimetype-content-wrapper.ts
@@ -5,10 +5,12 @@ export class MimetypeContentWrapper {
     mimetype: string[],
     obj: Record<string, any>
   ): Record<'content', ContentObject> {
-    const content = mimetype.reduce(
-      (acc, item) => ({ ...acc, [item]: obj }),
-      {}
-    );
+    const content = mimetype.reduce((acc, item) => {
+      if (obj.examples === undefined) {
+        delete obj.examples;
+      }
+      return { ...acc, [item]: obj };
+    }, {});
     return { content };
   }
 }
